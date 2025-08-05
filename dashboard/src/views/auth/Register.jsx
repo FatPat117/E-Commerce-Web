1c19acfc834e538912fd29cf338ed39b9fdd6bc6
Phat117 <ntphat110705@gmail.com>
2025-08-05 22:59:48 +0700

    Register Page State Define

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import { FaFacebook, FaGoogle } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaFacebook, FaGoogle, FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+        const [hiddenPassword, setHiddenPassword] = useState(true);
+        const [state, setState] = useState({
+                name: "",
+                email: "",
+                password: "",
+        });
+
+        const inputHandle = (e) => {
+                setState({ ...state, [e.target.name]: e.target.value });
+        };
+
+        const handleSubmit = (e) => {
+                e.preventDefault();
+                console.log(state);
+        };
+
+        const togglePassword = () => {
+                setHiddenPassword(!hiddenPassword);
+        };
         return (
                 <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center ">
                         <div className="w-[450px] text-white p-2 py-10 relative">
@@ -16,7 +35,7 @@ const Register = () => {
                                         </p>
 
                                         {/* Form */}
-                                        <form className="flex flex-col gap-x-5 gap-y-4">
+                                        <form className="flex flex-col gap-x-5 gap-y-4" onSubmit={handleSubmit}>
                                                 {/* Name */}
                                                 <div className="flex flex-col w-full gap-1 ">
                                                         <label htmlFor="name" className="text-md font-medium">
@@ -27,6 +46,8 @@ const Register = () => {
                                                                 id="name"
                                                                 name="name"
                                                                 placeholder="Enter your name"
+                                                                value={state.name}
+                                                                onChange={inputHandle}
                                                                 required
                                                                 className="w-full px-3 py-2 outline-none rounded-md border border-white/90 bg-transparent "
                                                         />
@@ -42,24 +63,34 @@ const Register = () => {
                                                                 id="email"
                                                                 name="email"
                                                                 placeholder="Enter your email"
+                                                                value={state.email}
+                                                                onChange={inputHandle}
                                                                 required
                                                                 className="w-full px-3 py-2 outline-none rounded-md border border-white/90 bg-transparent "
                                                         />
                                                 </div>
 
                                                 {/* Password    */}
-                                                <div className="flex flex-col w-full gap-1 ">
+                                                <div className="flex flex-col w-full gap-1 relative     ">
                                                         <label htmlFor="password" className="text-md font-medium">
                                                                 Password
                                                         </label>
                                                         <input
-                                                                type="password"
+                                                                type={hiddenPassword ? "password" : "text"}
                                                                 id="password"
                                                                 name="password"
                                                                 placeholder="Enter your password"
+                                                                value={state.password}
+                                                                onChange={inputHandle}
                                                                 required
                                                                 className="w-full px-3 py-2 outline-none rounded-md border border-white/90 bg-transparent "
                                                         />
+                                                        <span
+                                                                onClick={togglePassword}
+                                                                className="absolute top-[50%] right-[3px] translate-x-[-50%] translate-y-[40%] cursor-pointer"
+                                                        >
+                                                                {hiddenPassword ? <FaEye /> : <FaEyeSlash />}
+                                                        </span>
                                                 </div>
 
                                                 {/* Checkbox */}
