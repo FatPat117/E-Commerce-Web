1aaab80ad84e580e3fecabad1a81cdecbc98d850
Phat117 <ntphat110705@gmail.com>
2025-08-07 23:03:35 +0700

    Setting up the Layout Part 4: Hide and Show SideBar Function

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { FaList } from "react-icons/fa";
 
-const Header = () => {
+const Header = ({ showSidebar, setShowSidebar }) => {
         return (
-                <div>
-                        <h1>Header </h1>
-                </div>
+                <header className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
+                        <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-center bg-[#b1addf] px-5 transition-all ">
+                                <div
+                                        onClick={() => setShowSidebar(!showSidebar)}
+                                        className="cursor-pointer w-[35px] flex lg:hidden h-[35px] rounded-md bg-indigo-500 shadow-lg hover:shadow-indigo-400/50 transition-all justify-center items-center "
+                                >
+                                        <span>
+                                                <FaList />
+                                        </span>
+                                </div>
+                        </div>
+                </header>
         );
 };
 
--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { BiLogOutCircle } from "react-icons/bi";
+import { FaTimes } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 import { getNav } from "../navigation";
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar, setShowSidebar }) => {
         const { pathname } = useLocation();
         const [allNav, setAllNav] = useState([]);
 
@@ -13,11 +14,18 @@ const Sidebar = () => {
         }, []);
 
         return (
-                <div>
-                        <div></div>
+                <aside className="relative">
+                        <div
+                                onClick={() => setShowSidebar(false)}
+                                className={`${
+                                        showSidebar ? "invisible" : "visible"
+                                } flex duration-200 w-screen h-screen bg-[#8cbce780] top-0 left-0 z-10`}
+                        ></div>
 
                         <div
-                                className={`w-[280px] fixed bg-[#e6e7fb] z-50 top-0  h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+                                className={`w-[280px] fixed bg-[#e6e7fb] z-50 top-0  h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
+                                        showSidebar ? "left-0" : "left-[-280px] lg:left-0"
+                                }`}
                         >
                                 <div className="h-[70px] flex justify-center items-center ">
                                         <Link to="/" className="w-[180px] h-[60px]">
@@ -25,7 +33,16 @@ const Sidebar = () => {
                                         </Link>
                                 </div>
 
-                                <div className="px-[16px] ">
+                                <div className="px-[16px]   ">
+                                        <div
+                                                onClick={() => setShowSidebar(false)}
+                                                className="absolute top-0 right-0 cursor-pointer p-2 lg:hidden"
+                                        >
+                                                <span>
+                                                        <FaTimes size={20} />
+                                                </span>
+                                        </div>
+
                                         <ul>
                                                 {allNav.map((nav, idx) => (
                                                         <li key={idx}>
@@ -57,7 +74,7 @@ const Sidebar = () => {
                                         </ul>
                                 </div>
                         </div>
-                </div>
+                </aside>
         );
 };
 
