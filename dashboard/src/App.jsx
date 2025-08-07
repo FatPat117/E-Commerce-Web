f6a501542fd5379339d34deb2014a787c6f96a06
Phat117 <ntphat110705@gmail.com>
2025-08-07 21:39:29 +0700

    Middleware And Routes Manage Part 3: Define Admin and Seller Routes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+// App.js đã sửa lỗi logic
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-
 import { default as Router } from "./router/Router";
+import { getRoutes } from "./router/routes/index.jsx";
 import publicRoutes from "./router/routes/publicRoutes";
 
-const App = () => {
+function App() {
         const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+
+        useEffect(() => {
+                const routes = getRoutes();
+
+                setAllRoutes((prevRoutes) => [...prevRoutes, ...routes]);
+        }, []);
 
         return (
                 <BrowserRouter>
                         <Router allRoutes={allRoutes} />
                 </BrowserRouter>
         );
-};
+}
 
 export default App;
--- dashboard/src/router/routes/sellerRoutes.jsx
+++ dashboard/src/router/routes/sellerRoutes.jsx
@@ -1,5 +1,4 @@
 import { lazy } from "react";
-
 const Home = lazy(() => import("../../views/Home"));
 
 export const sellerRoutes = [
