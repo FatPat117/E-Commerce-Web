8e51669b977ba335b956b7812b552f3d8e7da19d
Phat117 <ntphat110705@gmail.com>
2025-08-07 21:54:55 +0700

    Setting up the Layout Part 1: Set up the MainLayout

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
         return (
                 <div>
-                        <h1>SideBar</h1>
+                        <div></div>
+
+                        <div
+                                className={`w-[280px] fixed bg-[#e6e7fb] z-50 top-0  h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all`}
+                        >
+                                <div className="h-[70px] flex justify-center items-center ">
+                                        <Link to="/" className="w-[180px] h-[50px]">
+                                                <img src="/images/logo.png" alt="logo" className="w-full h-full" />
+                                        </Link>
+                                </div>
+                        </div>
                 </div>
         );
 };
