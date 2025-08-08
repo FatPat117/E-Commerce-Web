ab7b93be670b0ae19c8feb8dc418d0642a716bbf
Phat117 <ntphat110705@gmail.com>
2025-08-08 21:05:09 +0700

    Fixed Bug: SideBar Overlay

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -14,33 +14,33 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
         }, []);
 
         return (
-                <aside className="relative">
+                <>
+                        {/* Overlay */}
                         <div
                                 onClick={() => setShowSidebar(false)}
                                 className={`${
-                                        showSidebar ? "visible" : "invisible "
-                                } flex duration-200 w-screen h-screen bg-[#8cbce780] top-0 left-0 z-50 lg:hidden `}
+                                        showSidebar ? "visible opacity-100" : "invisible opacity-0"
+                                } fixed top-0 left-0 w-screen h-screen bg-[#8cbce780] transition-opacity duration-200 z-40 lg:hidden`}
                         ></div>
 
-                        <div
-                                className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0  h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
+                        {/* Sidebar */}
+                        <aside
+                                className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${
                                         showSidebar ? "left-0" : "left-[-280px] lg:left-0"
                                 }`}
                         >
-                                <div className="h-[70px] flex justify-center items-center ">
+                                <div className="h-[70px] flex justify-center items-center">
                                         <Link to="/" className="w-[180px] h-[60px]">
                                                 <img src="/images/logo.png" alt="logo" className="w-full h-full" />
                                         </Link>
                                 </div>
 
-                                <div className="px-[16px]   ">
+                                <div className="px-[16px] ">
                                         <div
                                                 onClick={() => setShowSidebar(false)}
-                                                className="absolute top-0 right-0 cursor-pointer p-2 lg:hidden"
+                                                className="absolute top-2 right-0 cursor-pointer p-2 lg:hidden"
                                         >
-                                                <span>
-                                                        <FaTimes size={20} />
-                                                </span>
+                                                <FaTimes size={20} />
                                         </div>
 
                                         <ul>
@@ -65,16 +65,14 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
                                                                 to="/"
                                                                 className="text-[#030811] font-bold duration-200 px-[12px] py-[12px] rounded-md flex justify-start text-md md:text-lg items-center gap-3 hover:pl-6 transition-all w-full mb-1"
                                                         >
-                                                                <span>
-                                                                        <BiLogOutCircle />
-                                                                </span>
+                                                                <BiLogOutCircle />
                                                                 <span>Logout</span>
                                                         </Link>
                                                 </li>
                                         </ul>
                                 </div>
-                        </div>
-                </aside>
+                        </aside>
+                </>
         );
 };
 
