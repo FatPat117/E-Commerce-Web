c8f99d17bee4cfced518dedb5c679925a528794a
Phat117 <ntphat110705@gmail.com>
2025-08-10 18:25:21 +0700

    Sellers Chat Page Design Part 5: Responsive Message Box Chat

--- dashboard/src/views/admin/ChatSeller.jsx
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -82,7 +82,39 @@ const ChatSeller = () => {
 
                                                 {/* Message Box */}
                                                 <div className="py-4 ">
-                                                        <div className="bg-[#475569] h-[calc(100vh-300px)] rounded-md p-3 overflow-y-auto"></div>
+                                                        <div className="bg-[#475569] h-[calc(100vh-300px)] rounded-md p-3 overflow-y-auto">
+                                                                {/* Other Message   */}
+                                                                <div className="w-full flex justify-start items-center">
+                                                                        <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
+                                                                                <div>
+                                                                                        <img
+                                                                                                src="/images/admin.jpg"
+                                                                                                alt="avatar"
+                                                                                                className="w-[38px] h-[38px] rounded-full border-white border-2 max-w-[38px] p-[3px] "
+                                                                                        />
+                                                                                </div>
+                                                                                <div className="flex justify-center items-start flex-col w-full bg-blue-500 shadow-md shadow-blue-500/50 text-white py-1 px-2 rounded-sm ">
+                                                                                        <span>How are you ? </span>
+                                                                                </div>
+                                                                        </div>
+                                                                </div>
+
+                                                                {/* My  Message */}
+                                                                <div className="w-full flex justify-end items-center">
+                                                                        <div className="flex justify-start items-start gap-2 md:px-3 py-2 max-w-full lg:max-w-[85%]">
+                                                                                <div className="flex justify-center items-start flex-col w-full bg-red-500 shadow-md shadow-red-500/50 text-white py-1 px-2 rounded-sm ">
+                                                                                        <span>How are you ? </span>
+                                                                                </div>
+                                                                                <div>
+                                                                                        <img
+                                                                                                src="/images/admin.jpg"
+                                                                                                alt="avatar"
+                                                                                                className="w-[38px] h-[38px] rounded-full border-white border-2 max-w-[38px] p-[3px] "
+                                                                                        />
+                                                                                </div>
+                                                                        </div>
+                                                                </div>
+                                                        </div>
                                                 </div>
 
                                                 {/* Button  */}
