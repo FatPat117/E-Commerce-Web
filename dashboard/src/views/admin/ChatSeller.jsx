6987405b97a405b252bd05286e1c455c4a1c46d8
Phat117 <ntphat110705@gmail.com>
2025-08-10 17:43:11 +0700

    Sellers Chat Page Design Part 2: SideBar Chat List Online or Not

--- dashboard/src/views/admin/ChatSeller.jsx
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -11,15 +11,42 @@ const ChatSeller = () => {
                                         {/* Sidebar */}
                                         <div
                                                 className={`w-[280px] h-full absolute z-10 ${
-                                                        show ? "-left-[16px]" : "-left-[0px]"
+                                                        show ? "-left-[16px]" : "-left-[336px]"
                                                 } md:left-0 md:relative transition-all`}
                                         >
                                                 <div className="w-full h-[calc(100vh-177px)] bg-[#9e99e9] md:bg-transparent overflow-y-auto">
                                                         <div className="flex text-xl justify-between items-center p-4 md:p-0 md:px-3 md:pb-3 text-white">
                                                                 <h2>Seller</h2>
-                                                                <span className="block cursor-pointer md:hidden">
+                                                                <span
+                                                                        className="block cursor-pointer md:hidden"
+                                                                        onClick={() => setShow(!show)}
+                                                                >
                                                                         <IoMdClose />
                                                                 </span>
+                                                        </div>
+
+                                                        {/* Chat list */}
+                                                        <div
+                                                                className={
+                                                                        "h-[60px] flex items-center justify-start gap-3 text-white p-2 rounded-sm cursor-pointer"
+                                                                }
+                                                        >
+                                                                <div className="relative">
+                                                                        <img
+                                                                                src="/images/admin.jpg"
+                                                                                alt="avatar"
+                                                                                className="w-[38px] h-[38px] rounded-full border-white border-2 max-w-[38px] p-[2px] "
+                                                                        />
+                                                                        <div className="w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0"></div>
+                                                                </div>
+
+                                                                <div className="flex justify-center items-start flex-col w-full">
+                                                                        <div className="flex justify-between items-center w-full">
+                                                                                <h2 className="text-base font-semibold">
+                                                                                        Pitachiti
+                                                                                </h2>
+                                                                        </div>
+                                                                </div>
                                                         </div>
                                                 </div>
                                         </div>
