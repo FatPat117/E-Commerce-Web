78dbb8871fc19b0f41c873e5a41a039146d9a099
Phat117 <ntphat110705@gmail.com>
2025-08-10 17:32:56 +0700

    Sellers Chat Page Design Part 1: SideBar Chat

---
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+
+const ChatSeller = () => {
+        const [show, setShow] = useState(false);
+
+        return (
+                <div className="px-2 lg:px-7 pt-5">
+                        <div className="w-full p-4 bg-[#6a5fdf] rounded-md  h-[calc(100vh-140px)]">
+                                <div className="flex w-full h-full relative">
+                                        {/* Sidebar */}
+                                        <div
+                                                className={`w-[280px] h-full absolute z-10 ${
+                                                        show ? "-left-[16px]" : "-left-[0px]"
+                                                } md:left-0 md:relative transition-all`}
+                                        >
+                                                <div className="w-full h-[calc(100vh-177px)] bg-[#9e99e9] md:bg-transparent overflow-y-auto">
+                                                        <div className="flex text-xl justify-between items-center p-4 md:p-0 md:px-3 md:pb-3 text-white">
+                                                                <h2>Seller</h2>
+                                                                <span className="block cursor-pointer md:hidden">
+                                                                        <IoMdClose />
+                                                                </span>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        );
+};
+
+export default ChatSeller;
