86732476d7e664e7408f6f69a411dd3d5d72c712
Phat117 <ntphat110705@gmail.com>
2025-08-18 21:00:31 +0700

    Header Design Part 3: Menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { FaFacebookF, FaLinkedin, FaLock } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaFacebookF, FaLinkedin, FaList, FaLock } from "react-icons/fa";
 import { FaInstagram, FaUser } from "react-icons/fa6";
 import { IoIosArrowDown, IoMdPhonePortrait } from "react-icons/io";
 import { MdEmail } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const Header = () => {
+        const { pathname } = useLocation();
+        const [showSidebar, setShowSidebar] = useState(false);
         const user = true;
 
         return (
                 <header className="w-full bg-white">
+                        {/* Header top */}
                         <div className="header-top bg-[#caddff] md-lg:block hidden ">
                                 <div className="w-[85%] lg:w-[90%] mx-auto">
                                         <div className="flex w-full justify-between items-center h-[50px] text-solid-500">
@@ -88,6 +91,100 @@ const Header = () => {
                                                                 )}
                                                         </div>
                                                 </div>
+                                        </div>
+                                </div>
+                        </div>
+
+                        {/* Main header */}
+                        <div className="bg-white">
+                                <div className="w-[85%] lg:w-[90%] mx-auto">
+                                        <div className="h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap">
+                                                {/* logo */}
+                                                <div className="md-lg:w-3/12 w-full md-lg:pt-4 ">
+                                                        <div className="flex justify-between items-center">
+                                                                <Link to="/">
+                                                                        <img src="/images/logo.png" alt="logo" />
+                                                                </Link>
+
+                                                                {/* Hidden sidebar */}
+                                                                <div
+                                                                        onClick={() => setShowSidebar(!showSidebar)}
+                                                                        className="flex justify-center items-center w-[30px] h-[30px] bg-white text-slate-700 border border-slate-600 rounded-sm cursor-pointer  md-lg:hidden"
+                                                                >
+                                                                        <span>
+                                                                                <FaList />
+                                                                        </span>
+                                                                </div>
+                                                        </div>
+                                                </div>
+
+                                                {/* Menu */}
+                                                <nav className="md-lg:w-9/12 w-full">
+                                                        <div className="flex md-lg:justify-between justify-center items-center flex-wrap pl-8">
+                                                                <ul className=" justify-start items-start gap-8 text-sm font-bold uppercase hidden md-lg:flex">
+                                                                        <li>
+                                                                                <Link
+                                                                                        className={`p-2 block ${
+                                                                                                pathname === "/"
+                                                                                                        ? "text-[#059473]"
+                                                                                                        : "text-slate-600"
+                                                                                        }`}
+                                                                                        to="/"
+                                                                                >
+                                                                                        Home
+                                                                                </Link>
+                                                                        </li>
+                                                                        <li>
+                                                                                <Link
+                                                                                        className={`p-2 block ${
+                                                                                                pathname === "/shop"
+                                                                                                        ? "text-[#059473]"
+                                                                                                        : "text-slate-600"
+                                                                                        }`}
+                                                                                        to="/shop"
+                                                                                >
+                                                                                        Shop
+                                                                                </Link>
+                                                                        </li>
+                                                                        <li>
+                                                                                <Link
+                                                                                        className={`p-2 block ${
+                                                                                                pathname === "/blog"
+                                                                                                        ? "text-[#059473]"
+                                                                                                        : "text-slate-600"
+                                                                                        }`}
+                                                                                        to="/blog"
+                                                                                >
+                                                                                        Blog
+                                                                                </Link>
+                                                                        </li>
+                                                                        <li>
+                                                                                <Link
+                                                                                        className={`p-2 block ${
+                                                                                                pathname === "/about"
+                                                                                                        ? "text-[#059473]"
+                                                                                                        : "text-slate-600"
+                                                                                        }`}
+                                                                                        to="/shop"
+                                                                                >
+                                                                                        About Us
+                                                                                </Link>
+                                                                        </li>
+                                                                        <li>
+                                                                                <Link
+                                                                                        className={`p-2 block ${
+                                                                                                pathname === "/contact"
+                                                                                                        ? "text-[#059473]"
+                                                                                                        : "text-slate-600"
+                                                                                        }`}
+                                                                                        to="/shop"
+                                                                                >
+                                                                                        Contact us
+                                                                                </Link>
+                                                                        </li>
+                                                                </ul>
+                                                        </div>
+                                                </nav>
                                         </div>
                                 </div>
                         </div>
