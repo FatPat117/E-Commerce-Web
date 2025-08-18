13b0e658488fefe0bb7fb90353947ea823b5a8ad
Phat117 <ntphat110705@gmail.com>
2025-08-18 19:17:22 +0700

    Project Setup,Folder ,Route Manage Part 1: Set up for the front-end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+        {
+                path: "/",
+                element: <Home />,
+        },
+]);
+const App = () => {
+        return <RouterProvider router={router}></RouterProvider>;
+};
+
+export default App;
