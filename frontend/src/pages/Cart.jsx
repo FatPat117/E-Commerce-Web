d30d4acc93ada9274f467e78aa1b46b180edd61b
Phat117 <ntphat110705@gmail.com>
2025-08-20 20:44:27 +0700

    Cart Page Design Part 3: Order Summary

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -35,6 +35,7 @@ const Cart = () => {
                                 <div className="w-[85%] md:w-[90%] mx-auto py-16">
                                         {cartProducts.length > 0 || outOfStockProducts.length > 0 ? (
                                                 <div className="flex flex-wrap">
+                                                        {/* Left Order info */}
                                                         <div className="md-lg:w-[67%] w-full mx">
                                                                 <div className="pr-0 md-lg:pr-3">
                                                                         <div className="flex flex-col gap-3 gap-y-5">
@@ -218,6 +219,65 @@ const Cart = () => {
                                                                                         </div>
                                                                                 )}
                                                                         </div>
+                                                                </div>
+                                                        </div>
+
+                                                        {/* Right Order Summary */}
+                                                        <div className="w-full md-lg:w-[33%]">
+                                                                <div className="pl-0 md-lg:pl-3 pt-5">
+                                                                        {cartProducts.length > 0 && (
+                                                                                <div className="bg-white p-3 text-slate-600 flex flex-col gap-3">
+                                                                                        <h2 className="text-xl font-bold">
+                                                                                                Order Summary
+                                                                                        </h2>
+                                                                                        {/* Items */}
+                                                                                        <div className="flex justify-between items-center">
+                                                                                                <span className="font-semibold">
+                                                                                                        2 Items
+                                                                                                </span>
+                                                                                                <span className="">
+                                                                                                        $343
+                                                                                                </span>
+                                                                                        </div>
+
+                                                                                        {/* Shopping Fee */}
+                                                                                        <div className="flex justify-between items-center">
+                                                                                                <span className="font-semibold">
+                                                                                                        Shopping Fee:
+                                                                                                </span>
+                                                                                                <span> $343</span>
+                                                                                        </div>
+
+                                                                                        {/* Voucher Coupon */}
+                                                                                        <div className="flex gap-2">
+                                                                                                <input
+                                                                                                        type="text"
+                                                                                                        name=""
+                                                                                                        id=""
+                                                                                                        placeholder="Voucher Coupon"
+                                                                                                        className="w-full px-3 py-2 border border-slate-200 outline-none focus:border-green-500 rounded-sm"
+                                                                                                />
+                                                                                                <button className="px-5 py-[1px] bg-[#059473] text-white rounded-sm uppercase text-md font-bold">
+                                                                                                        Apply
+                                                                                                </button>
+                                                                                        </div>
+
+                                                                                        {/* Total */}
+                                                                                        <div className="flex justify-between items-center mt-4">
+                                                                                                <span className="font-semibold text-xl">
+                                                                                                        Total:
+                                                                                                </span>
+                                                                                                <span className="font-bold text-xl text-[#059473]">
+                                                                                                        {" "}
+                                                                                                        $343
+                                                                                                </span>
+                                                                                        </div>
+
+                                                                                        <button className="cursor-pointer px-5 py-[8px] rounded-sm hover:shadow-red-500/50 hover:shadow-md font-semibold bg-red-500 text-md text-white uppercase">
+                                                                                                Process to Checkout
+                                                                                        </button>
+                                                                                </div>
+                                                                        )}
                                                                 </div>
                                                         </div>
                                                 </div>
