4708108edeb1dcd0182bd0ce5e7e3a736e606b23
Phat117 <ntphat110705@gmail.com>
2025-08-20 15:24:35 +0700

    Setup Shop Page Design Part 2: Category Checkbox

--- frontend/src/pages/Shops.jsx
+++ frontend/src/pages/Shops.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { URL } from "../utils/utils";
 const Shops = () => {
+        const [filter, setFilter] = useState(true);
+        const categories = [
+                "Mobiles",
+                "Laptops",
+                "Speakers",
+                "Top Wear",
+                "Footwear",
+                "Watches",
+                "Home Decor",
+                "Smart Watches",
+        ];
+
         return (
                 <div>
+                        {/* Banner */}
                         <section
                                 style={{ backgroundImage: `url(${URL}/images/banner/shop.png)` }}
                                 className={` h-[220px] bg-cover bg-no-repeat relative bg-left`}
@@ -19,6 +32,58 @@ const Shops = () => {
                                                                         <IoIosArrowForward />
                                                                 </span>
                                                                 <span className="underline text-blue-300">Shop</span>
+                                                        </div>
+                                                </div>
+                                        </div>
+                                </div>
+                        </section>
+
+                        <section className="py-16">
+                                <div className="w-[80%] md-lg:w-[85%] lg:w-[90%] h-full mx-auto">
+                                        {/* Filter Product Show in Mobile*/}
+                                        <div className={`md:hidden block ${filter ? "mb-6" : "mb-0"}`}>
+                                                <button
+                                                        onClick={() => setFilter(!filter)}
+                                                        className="text-center w-full py-2 px-3 bg-indigo-500 text-white"
+                                                >
+                                                        Filter Product
+                                                </button>
+                                        </div>
+
+                                        {/* Category Search And Product*/}
+                                        <div className="w-full flex flex-wrap">
+                                                {/* Category search */}
+                                                <div
+                                                        className={`w-full md:w-4/12 md-lg:w-3/12 pr-8 ${
+                                                                filter
+                                                                        ? " h-auto overflow-auto mb-0"
+                                                                        : " h-0 overflow-hidden mb-6 md:h-auto md:overflow-auto md:mb-0"
+                                                        }`}
+                                                >
+                                                        <h2 className="text-3xl font-bold mb-3 text-slate-600">
+                                                                Category
+                                                        </h2>
+                                                        <div className="py-2">
+                                                                {categories.map((cate, idx) => {
+                                                                        return (
+                                                                                <div
+                                                                                        key={idx}
+                                                                                        className="flex justify-start items-center gap-2 py-1"
+                                                                                >
+                                                                                        <input
+                                                                                                type="checkbox"
+                                                                                                name=""
+                                                                                                id={cate}
+                                                                                        />
+                                                                                        <label
+                                                                                                htmlFor={cate}
+                                                                                                className="text-slate-600 block cursor-pointer"
+                                                                                        >
+                                                                                                {cate}
+                                                                                        </label>
+                                                                                </div>
+                                                                        );
+                                                                })}
                                                         </div>
                                                 </div>
                                         </div>
