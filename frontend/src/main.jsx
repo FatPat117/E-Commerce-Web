d313d683845d8870f0d1e56661096f9a51b9b01f
Phat117 <ntphat110705@gmail.com>
2025-08-22 22:21:02 +0700

    Redux Toolkit Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
+import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
 import "./index.css";
-
+import store from "./store";
+const App = lazy(() => import("./App.jsx"));
 createRoot(document.getElementById("root")).render(
         <StrictMode>
-                <App />
+                <Suspense fallback={<div>Loading...</div>}>
+                        <Provider store={store}>
+                                <App />
+                                <Toaster
+                                        toastOptions={{
+                                                position: "top-right",
+                                                style: {
+                                                        background: "#283046",
+                                                        color: "#ffffff",
+                                                },
+                                        }}
+                                />
+                        </Provider>
+                </Suspense>
         </StrictMode>
 );
