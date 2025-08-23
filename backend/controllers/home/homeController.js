f2cfc447485c7803606ab4ced07726f9a841b492
Phat117 <ntphat110705@gmail.com>
2025-08-23 21:45:57 +0700

    Product Query And Show Part 4: Build the Query Products Class

--- backend/controllers/home/homeController.js
+++ backend/controllers/home/homeController.js
@@ -77,8 +77,8 @@ const price_range_latest_product = asyncHandler(async (req, res, next) => {
 });
 
 const query_products = asyncHandler(async (req, res, next) => {
+        req.query.perPage = req.query.perPage || 2;
         const { category, rating, sortByPrice, lowPrice, highPrice, pageNumber } = req.query;
-        const limit = 12;
 
         const products = await Product.find({}).sort({ createdAt: -1 });
         const totalProduct = await Product.countDocuments({});
--- backend/utils/queryProducts.js
+++ backend/utils/queryProducts.js
@@ -24,9 +24,45 @@ class QueryProducts {
                 this.products = this.query.price
                         ? this.products.filter(
                                   (product) =>
-                                          product.price >= this.query.price[0] && product.price <= this.query.price[1]
+                                          product.price >= this.query.lowPrice && product.price <= this.query.highPrice
                           )
                         : this.products;
+                return this;
+        }
+
+        sortByPriceQuery() {
+                if (this.query.sortByPrice === "low-to-high") {
+                        this.products = this.products.sort((a, b) => a.price - b.price);
+                } else if (this.query.sortByPrice === "high-to-low") {
+                        this.products = this.products.sort((a, b) => b.price - a.price);
+                }
+                return this;
+        }
+
+        skipQuery() {
+                let { pageNumber, perPage } = this.query;
+                const skipPage = parseInt(pageNumber - 1) * parseInt(perPage);
+                const skipProduct = [];
+                for (let i = skipPage; i < this.products.length; i++) {
+                        skipProduct.push(this.products[i]);
+                }
+                this.products = skipProduct;
+                return this;
+        }
+
+        limitQuery() {
+                let { perPage } = this.query;
+                if (this.products.length > perPage) {
+                        this.products = this.products.slice(0, parseInt(perPage));
+                }
+                return this;
+        }
+
+        getProducts() {
+                return this.products;
+        }
+        getTotalProducts() {
+                return this.products.length;
         }
 }
 
