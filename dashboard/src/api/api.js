4b46e53027064acf399fb00a9d08888b984d8752
Phat117 <ntphat110705@gmail.com>
2025-09-12 19:57:44 +0700

    Fix api in Dashboard for Production

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
+const localUrl = "http://localhost:5000";
+const productionUrl = "https://e-commerce-web-8l3q.onrender.com";
 
+let API_URL = "";
+let mode = "pro";
+
+if (mode == "pro") {
+        API_URL = productionUrl;
+} else {
+        API_URL = localUrl;
+}
 const api = axios.create({
-        baseURL: "http://localhost:5000/api",
+        baseURL: `${API_URL}/api`,
         withCredentials: true,
 });
 
